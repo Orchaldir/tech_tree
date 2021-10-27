f0588b8a82fa87cfbfc9c1969ae0432de44f2cdc
Orchaldir <orchaldir@gmail.com>
2021-10-27 21:48:51 +0200

    [#7] Improve create_svg()

--- tech_tree_svg/tests/test.rs
+++ tech_tree_svg/tests/test.rs
@@ -5,33 +5,44 @@ use svg::Document;
 
 #[test]
 fn create_svg() {
-    let data = Data::new()
-        .move_to((10, 10))
-        .line_by((0, 50))
-        .line_by((50, 0))
-        .line_by((0, -50))
+    let x = 35i32;
+    let y = 35i32;
+    let font_size = 10i32;
+    let font_width = font_size / 2;
+    let padding = font_size;
+    let text = "Hello";
+    let text_offset = font_size / 3;
+    let width = text.len() as i32 * font_width + 2 * padding;
+    let width_half = width / 2;
+    let height = font_size * 2;
+
+    let box_data = Data::new()
+        .move_to((x - width_half, y - font_size))
+        .line_by((width, 0))
+        .line_by((0, height))
+        .line_by((-width, 0))
         .close();
 
-    let path = Path::new()
+    let box_node = Path::new()
         .set("fill", "none")
         .set("stroke", "black")
         .set("stroke-width", 1)
-        .set("d", data);
+        .set("d", box_data);
 
-    let element = svg::node::Text::new("Hello");
+    let text_element = svg::node::Text::new(text);
 
-    let text = Text::new()
-        .set("x", 35)
-        .set("y", 35)
-        .set("font-size", 10)
+    let text_node = Text::new()
+        .set("x", x)
+        .set("y", y + text_offset)
+        .set("font-size", font_size)
         .set("text-anchor", "middle")
-        .set("dominant-baseline", "middle")
-        .add(element);
+        //.set("dominant-baseline", "middle")
+        .add(text_element);
 
     let document = Document::new()
         .set("viewBox", (0, 0, 70, 70))
-        .add(path)
-        .add(text);
+        .add(box_node)
+        .add(text_node);
 
     svg::save("image.svg", &document).unwrap();
 }
